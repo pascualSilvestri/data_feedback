d35a818a5cce02a4d9412cd728e4c8bdb82ecce0
pascual_silvestri <pascualsilvestri14@gmail.com>
2023-07-15 16:34:15 -0300

    Update firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -5,8 +5,10 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const apiKey = import.meta.env.VITE_API_KEY_FIREDB
+
 const firebaseConfig = {
-  apiKey: "AIzaSyDvqNbgQ7uNJcXkKa6S1XPA89NaQLHhjxo",
+  apiKey: apiKey,
   authDomain: "nativai.firebaseapp.com",
   projectId: "nativai",
   storageBucket: "nativai.appspot.com",
@@ -18,25 +20,4 @@ const firebaseConfig = {
 // Initialize Firebase
 export const fireDB = initializeApp(firebaseConfig);
 
-// const db = getFirestore(app);
 
-
-// const getCollectionData = async () => {
-//     try {
-//       const querySnapshot = await getDocs(collection(db, "feedbacks"));
-//       querySnapshot.forEach((doc) => {
-
-//         //obtener los comentarios de la bse de datos
-//         console.log(doc.data().comment)
-//         //Obteniendo la data de la base de datos 
-//         // console.log(doc.data());
-//       });
-//     } catch (error) {
-//       console.error("Error al obtener los datos de la colección:", error);
-//     }
-//   };
-  
-//   // Llamar a la función para obtener los datos de la colección
-//   getCollectionData();
-
-// console.log(app)
